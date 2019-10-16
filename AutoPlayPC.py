55a3419355fdc3a8b966ea4d59381aef10ef5564
abc <abc@abcdeMacBook-Air.local>
2019-10-16 09:52:52 +0800

    特性新增：添加贪吃蛇分支，实现基本图像信息提取功能

--- AutoPlayPC.py
+++ AutoPlayPC.py
@@ -7,11 +7,16 @@ import os
 import time
 import sys
 import traceback
-
-
+import math
+import cv2
+import matplotlib.pyplot as plt
+    # 'right_buttom_x': 698,
+    # 'right_buttom_y': 438,
+ctrl_center = (62, 373)
+Radius = 40
 @time_it
 def get_screenshot():
-    if sys.platform == 'win32':
+    if sys.platform == 'win32' or sys.platform == 'darwin':
         from PIL import ImageGrab
         scr = ImageGrab.grab(
             [loc['left_top_x'], loc['left_top_y'], loc['right_buttom_x'], loc['right_buttom_y']])
@@ -30,32 +35,99 @@ def get_screenshot():
         print('Unsupported platform: ', sys.platform)
         os._exit(0)
 
+def GetClickPos(theta, center, radius) :
+    x = center[0] + radius * math.cos(theta * math.pi / 180.0)
+    y = center[1] + radius * math.sin(theta * math.pi / 180.0)
+    return (x, y)
+
+def Region(img):
+    from skimage import data, util
+    from skimage.measure import label, regionprops
+    kernel = cv2.getStructuringElement(cv2.MORPH_ERODE,(5, 5))
+    eroded = cv2.erode(img,kernel)
+    #显示腐蚀后的图像
+    label_img = label(eroded, connectivity = 2)
+    props = regionprops(label_img)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    ax1.imshow(eroded, plt.cm.gray, interpolation='nearest')
+    ax1.axis('off')
+    ax2.imshow(label_img,interpolation='nearest')
+    ax2.axis('off')
+    fig.tight_layout()
+    plt.show()
+    # centroid of first labeled object props[0]['centroid']
+    return props
+
+def SortCentroid(c):
+    return c.bbox_area
+
+def CalThetah():
+    im = cv2.imread("./screenshot.png")
+    gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)  #把输入图像灰度化
+    #直接阈值化是对输入的单通道矩阵逐像素进行阈值分割。
+    ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    shape = gray.shape
+    radius = 70
+    im_crop = gray[int(shape[0] / 2 - radius) : int(shape[0] / 2 + radius), int(shape[1] / 2 - radius) : int(shape[1] / 2) + radius] 
+    im_crop_bin = binary[int(shape[0] / 2 - radius) : int(shape[0] / 2 + radius), int(shape[1] / 2 - radius) : int(shape[1] / 2) + radius]
+
+    centers = Region(im_crop_bin)
+
+    valid_centroid = []
+    for p in centers:
+        if p.bbox_area > 12:
+            valid_centroid.append(p)
+            # cv2.circle(im_crop, (int(p.centroid[1]), int(p.centroid[0])), 5, (0,255,0), 2)
+    valid_centroid = sorted(centers, key=SortCentroid)
+    cv2.circle(im_crop, (int(valid_centroid[-1].centroid[1]), int(valid_centroid[-1].centroid[0])), 5, (0,255,0), 2)
+    cv2.circle(im_crop, (int(valid_centroid[-2].centroid[1]), int(valid_centroid[-2].centroid[0])), 5, (0,255,0), 2)
+    cv2.circle(im_crop, (int(valid_centroid[-3].centroid[1]), int(valid_centroid[-3].centroid[0])), 5, (0,255,0), 2)
+
+    # cv2.namedWindow("liantiong", 1)
+    cv2.imshow("laintiong", im_crop)
+    cv2.waitKey(0)
+    # imgray=cv2.Canny(im_crop, 30, 100)
+
+    # circles = cv2.HoughCircles(imgray,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=10,maxRadius=30)
+    # for i in circles[0,:]:
+    # # draw the outer circle
+    #     cv2.circle(im_crop,(i[0],i[1]),i[2],(0,255,0),2)
+    # print("threshold value %s"%ret)
+    # cv2.namedWindow("binary0", 1)
+    # cv2.imshow("binary0", im_crop)
+    # cv2.waitKey(0)
 
 def Play():
     m = PyMouse()
-    flag = ""
-    while True:
+    theta = 0
+    cnt = 100
+    
+    clicked_pos = GetClickPos(theta, ctrl_center, Radius)
+    m.click(clicked_pos[0], clicked_pos[1])
+    m.press(clicked_pos[0], clicked_pos[1])
+    
+    while cnt > 0:
         # start = time.perf_counter()
-        time.sleep(0.1)
+        cnt -= 1
+        # time.sleep(0.1)
         try:
-            scr = get_screenshot()
-            expr = recognize(scr)
-            print(expr, eval(expr))
-            if flag == expr:
-                continue
-            else:
-                flag = expr
-                if eval(expr):
-                    m.click(loc['click_true_x'], loc['click_true_y'], 1)
-                else:
-                    m.click(loc['click_false_x'], loc['click_false_y'], 1)
+            # scr = get_screenshot()
+            # expr = recognize(scr)
+            theta += 45
+            move_pos = GetClickPos(theta, ctrl_center, Radius)
+            print(move_pos)
+            m.move(move_pos[0], move_pos[1])
+            
+            time.sleep(0.1)
+            print("Once!")
         except:
-            if 'scr' in vars():
-                scr.save('failed.png')
             print('Error occurred: ')
-            print(traceback.print_exc())
         # print('One loop: ', time.perf_counter() - a)
+
+    m.release(clicked_pos[0], clicked_pos[1])
 
 
 if __name__ == '__main__':
-    Play()
+    # Play()
+    CalThetah()
--- Config.py
+++ Config.py
@@ -1,10 +1,12 @@
 location_on_pc = {
-    'left_top_x': 100,
-    'left_top_y': 300,
-    'right_buttom_x': 400,
-    'right_buttom_y': 480,
-    'click_true_x': 150,
-    'click_true_y': 650,
+    'left_top_x': 1,
+    'left_top_y': 49,
+    'right_buttom_x': 698,
+    'right_buttom_y': 438,
+    'click_true_x': 66,
+    'click_true_y': 410,
     'click_false_x': 350,
-    'click_false_y': 650
+    'click_false_y': 650,
+    'self_x': 364,
+    'self_y': 240
 }
